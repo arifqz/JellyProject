96500b1c96d28ae9810cd556f6aed5efc3deedcf
arifqz <39536767+arifqz@users.noreply.github.com>
2020-10-25 13:46:42 -0400

    sdf
    sdf

--- sketch.js
+++ sketch.js
@@ -45,7 +45,11 @@ function mousePressed() {
 
 }
 
- 
+function pauseSketch() {
+  if (location.hash === '#?frame') {
+    noLoop();
+   console.log('pause sketch has run');
+  } 
 
 
 function draw() {
@@ -64,7 +68,7 @@ function draw() {
 //   }
 //   text(int(frameRate()),30,40);
 //   pop();
-
+pauseSketch();
   shapeys = [];
   for (let i = 0; i < poses.length; i++) {
   // For each pose detected, loop through all the keypoints
